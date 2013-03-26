97a812ea7cece0998c20454b4f360d45b1f9ca18
Otto Dietz <otto.dietz@physik.hu-berlin.de>
2013-03-26 15:26:21 +0100

    added q1d.py

---
+++ q1d.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Sonst ist a/b=0 für a=2 und b=1
+# aber a/b=0.5 für a=2. und b=1
+from __future__ import division
+
+from q1d_utils 		import *
+from q1d_smooth 		import *
+from q1d_loc_length 	import *
+
+if __name__ == "__main__":
+		  pass
