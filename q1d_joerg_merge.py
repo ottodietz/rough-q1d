b92db281f4240d5cf02aec9c96863eb19c4bdaa6
jdoppler <joerg.doppler@gmx.at>
2013-04-17 09:22:50 +0200

    minor changes

--- q1d_joerg_merge.py
+++ q1d_joerg_merge.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python
+
 import sys
-import getopt
 import argparse
+from math import *
 import numpy as np
 from numpy.fft import fft, fftshift
 from scipy.interpolate import interp1d
-from math import *
 import matplotlib.pyplot as plt
 
 # ---------------------------------------------
@@ -31,135 +32,159 @@ alpha_path = args.alpha_data
 exp_path = args.experimental_data
 num_path = args.numerical_data
 
-# ---------------------------------------------
+# -----------------------------------------------------------------------------
 # Constants
-# ---------------------------------------------
-alphaFile=alpha_path + "corred_and_shuffeled.dat"  # input file for boundary data
-expFile_corr, expFile_shuffle = [ exp_path + x + "_mode" + mode + "_kx.dat" for x in "rscb", "rsrb" ]
-numFile_corr, numFile_shuffle = [ num_path + "T" + 2*mode + "_num_" + x + ".dat" for x in "c", "s" ]
-#
-delta = 0.01                            # module width
-wireLength = 0.3                        # wire length L
-wireWidth = 0.1                         # wire width d
-#
-sampleLength = (wireLength+400.*delta)  # sampling length
-dx = .00005                             # step-length in x-space
-dk = 2.*pi/sampleLength                 # step-length in k-space
-xRange = np.arange(-sampleLength/2.,sampleLength/2.,dx)   # discretization of x-axis
-kRange = xRange*dk/dx                                     # discretization of k-axis
-kRangeNew = np.arange(0.01,200,0.25)                      # k-values for plot: (0,200)
+# -----------------------------------------------------------------------------
+alphaFile = alpha_path + "corred_and_shuffeled.dat"  # pinheight data
+expFiles = [ exp_path + x + "_mode" + mode + "_kx.dat" for x in "rscb", "rsrb" ]
+numFiles = [ num_path + "T" + 2*mode + "_num_" + x + ".dat" for x in "c", "s" ]
 
-# ---------------------------------------------
+pinSize = 0.005                             # pin size
+delta = 10*pinSize                          # module width
+wireLength = 50*delta                            # wire length L
+wireWidth = 0.1                             # wire width d
+
+samplingLength = (wireLength+400.*delta)    # sampling length
+dx = .0001                                 # step-length in x-space
+dk = 2.*pi/samplingLength                   # step-length in k-space
+xRange = np.arange(-samplingLength/2,
+                   samplingLength/2,dx)     # discretization x-axis
+kRange = xRange*dk/dx                      # discretization k-axis
+kRange = np.arange(0.01,600,0.01)
+kRangeNew = np.arange(0.01,600,0.01)        # k-values for plot
+
+# -----------------------------------------------------------------------------
 # Read data files and generate pin heights
-# ---------------------------------------------
-alpha = alpha_corr, alpha_shuffle = [ (x - x.mean()) for x in ( (0.8 + 0.2*y)/100 for
-                                            y in np.loadtxt(alphaFile,unpack=True) ) ]
-exp = exp_corr, exp_shuffle = [ np.loadtxt(i,unpack=True) for
-                                i in (expFile_corr,expFile_shuffle)]
-num = num_corr, num_shuffle = [ np.loadtxt(i,unpack=True) for
-                                i in (numFile_corr,numFile_shuffle)]
-#alpha_shuffle = np.random.uniform(-sqrt(3.),sqrt(3.), size=alpha_shuffle.size) # randomly chosen steps
+# -----------------------------------------------------------------------------
+alpha = [ (x - x.mean()) for x in
+          ( (0.8 + 0.2*y)/100 for y in np.loadtxt(alphaFile,unpack=True) ) ]
+exp = [ np.loadtxt(i,unpack=True) for i in expFiles ]
+num = [ np.loadtxt(i,unpack=True) for i in numFiles ]
+# randomly chosen steps:
+alpha_shuffle = np.random.uniform(-sqrt(3.)/100,sqrt(3.)/100, size=50)
+alpha_corr = np.random.uniform(-sqrt(3.)/100,sqrt(3.)/100, size=50)
+alpha = [ alpha_shuffle, alpha_corr ]
 
-# ---------------------------------------------
+# -----------------------------------------------------------------------------
 # Function definitions
-# ---------------------------------------------
+# -----------------------------------------------------------------------------
 def build_wire(x,alpha,delta,smearing):
-    "Return sum_(n=-N,N) alpha_n step(x-n*Delta)"
+    """
+    Return sum_(n=-N,N) alpha_n step(x-n*Delta)
+    """
     
     def step(x,delta,smearing):
-        "Return smoothed step-function Fermi(x-Delta)-Fermi(x)"
+        """
+        Return smoothed step-function Fermi(x-Delta)-Fermi(x)
+        """
         return 1./(1.+np.exp((x-delta)/smearing))-1./(1.+np.exp(x/smearing))
     
     module_number = alpha.size
     temp_wire = np.zeros(x.size)
     for n in np.arange(module_number):
-        temp_wire = ( temp_wire + alpha[n] *
-                        step(x - (n-module_number/2.)*delta,delta,smearing) )
+        temp_wire = (temp_wire + alpha[n]*
+                     step(x-(n-module_number/2.)*delta,delta,smearing))
     return temp_wire
 
 def fourier(data):
-    "Return Fourier transform of input data"
+    """
+    Return Fourier transform of input data
+    """
     return fftshift(abs(fft(data)*dx))**2
 
 def W_analytic(k,alpha,delta,smearing):
-    "Return roughness-height power spectrum W(k)"
+    """
+    Return roughness-height power spectrum W(k)
+    """
     module_index = np.arange(alpha.size)
     # Take correlation of alphas into account:
-    omega = ([ np.exp(-1j*n*k*delta)*alpha[n] for n in module_index] )
+    omega = ([ np.exp(-1j*n*k*delta)*alpha[n] for n in module_index])
     omega = np.sum(omega,axis=0)
     omega = abs(omega)**2 / alpha.size
-    return ( 1./delta * (2.*pi*smearing*
-                np.sinh(k*pi*smearing)**(-1)*np.sin(k*delta/2.))**2 ) * omega
+    return (1./delta * (2.*pi*smearing*
+            np.sinh(k*pi*smearing)**(-1)*np.sin(k*delta/2.))**2) * omega
 
 def S_analytic(k,alpha,delta,smearing):
-    "Return roughness-height power spectrum S(k)"
+    """
+    Return roughness-height power spectrum S(k)
+    """
     module_index = np.arange(alpha.size)
-    a = np.concatenate([ [0],alpha,[0] ])  # a[n-1] and a[n+1] = 0 for n=N and n=0
+    # a[n-1] and a[n+1] = 0 for n=N and n=0
+    a = np.concatenate([ [0],alpha,[0] ])  
     # Take correlation of alphas into account:
     omega = ([ np.exp(-1j*n*k*delta)*(a[n]*(a[n]-a[n+1])*
-                np.exp(-1j*k*delta) + a[n]*(a[n]-a[n-1])) for n in module_index] )
+               np.exp(-1j*k*delta) + a[n]*(a[n]-a[n-1])) for n in module_index])
     omega = np.sum(omega,axis=0)  
     omega = abs(omega)**2 / alpha.size 
     return ( 1./delta / 72. * (k*pi*(1.+k**2*smearing**2)*
-                            np.sinh(k*pi*smearing)**(-1))**2 ) * omega
+             np.sinh(k*pi*smearing)**(-1))**2 ) * omega
 
 def T_analytic(n,d,L,sigma,k,alpha,delta,smearing):
-    "Return transmission T based on analytical expressions for W and S"
-    invLbAGS=( 4.*sigma**2 / d**6 *(n*pi)**4 /
-                k**2 * W_analytic(2*k,alpha,delta,smearing) )
-    invLbSGS=( 0.5*(sigma/d*pi*n)**4 / k**2 *
-                (1./3. + 1./(pi*n)**2)**2 * S_analytic(2*k,alpha,delta,smearing) )
+    """
+    Return transmission T based on analytical expressions for W and S
+    """
+    invLbAGS=(4.*sigma**2 / d**6 *(n*pi)**4 /
+              k**2 * W_analytic(2*k,alpha,delta,smearing))
+    invLbSGS=(0.5*(sigma/d*pi*n)**4 / k**2 *
+              (1./3. + 1./(pi*n)**2)**2 * S_analytic(2*k,alpha,delta,smearing))
     return np.exp(-L*(invLbAGS+invLbSGS)), invLbAGS, invLbSGS
 
 def T_numeric(n,d,L,sigma,kOld,kNew,W,S):
-    "Return transmission T based on numerical expressions for W and S"
+    """
+    Return transmission T based on numerical expressions for W and S
+    """
     # W and S are symmetric w.r.t. k - use only positive k-values for interpolation:
     kOld, W, S = [ x[len(x)/2:] for x in (kOld, W, S)]
     # Find interpolating values at k-values kNew:
-    W, S = [ interp1d(kOld,x, kind="cubic")(2.*kNew) for x in (W, S) ]
+    W, S = [ interp1d(kOld,x, kind="cubic")(2.*kNew) for x in (W, S)]
     invLbAGS=4.*sigma**2 / d**6 *(n*pi)**4 / kNew**2 * W
     invLbSGS=0.5*(sigma/d*pi*n)**4 / kNew**2 * (1./3. + 1./(pi*n)**2)**2 * S
     return np.exp(-L*(invLbAGS+invLbSGS)), invLbAGS, invLbSGS
 
-# ---------------------------------------------
+# -----------------------------------------------------------------------------
 # Plot data: xi, W, S (analytic)
-# ---------------------------------------------
-if True:
-    # ---------------------------------------------
+# -----------------------------------------------------------------------------
+if False:
+    # -------------------------------------------------------------------------
     # Plot data: T (theory, simulation, experiment)
-    # ---------------------------------------------
-    for alpha, exp, num, subplotindex, label in zip( alpha, exp, num, [1,2], ["Correlated","Shuffled"] ):
+    # -------------------------------------------------------------------------
+    for alpha, exp, num, subplotindex, label in zip(alpha, exp, num, [1,2],
+                                                    ["Correlated","Shuffled"]):
         plt.subplot(2,1,subplotindex)
-        plt.semilogy(num[0],num[int(mode)+4],"r-o",label=r"$T_{%s}$ simulation" % (2*mode) )  # simulation results
-        plt.semilogy(exp[0],exp[1]*1000,"k-",label=r"$T_{%s}$ experiment ($\times 10^3$)" % (2*mode) )  # experimental results
+        plt.semilogy(num[0],num[int(mode)+4],"r-o",
+                     label=r"$T_{%s}$ simulation" % (2*mode))  
+        plt.semilogy(exp[0],exp[1]*1000,"k-",
+                     label=r"$T_{%s}$ experiment ($\times 10^3$)" % (2*mode))
         for s in smearing:
-            T,_,_ = T_analytic(int(mode),wireWidth,wireLength,1.,kRangeNew,alpha,delta,s)
-            plt.semilogy(kRangeNew,T,label=r"$T_{%s}$ ($\sigma$ = %g)" % (2*mode,s) )  # theory
+            T,_,_ = T_analytic(int(mode),wireWidth,wireLength,
+                               1.,kRangeNew,alpha,delta,s)
+            plt.semilogy(kRangeNew,T,
+                         label=r"$T_{%s}$ ($\sigma$ = %g)" % (2*mode,s)) 
         plt.title(label)
-        plt.xlim(0,100)
+        plt.xlim(0,400)
         plt.ylim(1e-7,1e1)
         plt.legend(loc=4)
         plt.ylabel(r"$T_{%s}$" % (2*mode),fontsize="x-large")
     plt.xlabel(r"$k_x$",fontsize="x-large")
     plt.show()
 
-# ---------------------------------------------
+# -----------------------------------------------------------------------------
 # Plot data: xi, W, S (numeric)
-# ---------------------------------------------
-if False:
+# -----------------------------------------------------------------------------
+if True:
     # ---------------------------------------------
     # Fourier transform for roughness-height power spectrum W(k)
     # ---------------------------------------------
     xi = xi_corr, xi_shuffle = [ build_wire(xRange,a,delta,smearing) for a in alpha ]  # discretized wire profile xi(x)
     W_numeric_corr, W_numeric_shuffle = [ fourier(i) / wireLength for i in xi ]  # Fourier transform wire profile xi and shift data (centered zero frequency)
-    W_analytic_corr, W_analytic_shuffle = [ W_analytic(kRange,a,delta,smearing) for a in alpha  ]  # analytical W(k)
+    W_analytic_corr, W_analytic_shuffle = [ W_analytic(kRange,a,delta,smearing[0]) for a in alpha  ]  # analytical W(k)
     
     # ---------------------------------------------
     # Fourier transform for square-gradient power spectrum S(k)
     # ---------------------------------------------
     xipsq = xipsq_corr, xipsq_shuffle = [ np.gradient(x/dx)**2 for x in xi ]  # discretized square gradient xi'(x)^2
     S_numeric_corr, S_numeric_shuffle = [ fourier(i) / wireLength / 2 for i in xipsq ]  # factor 1/2 from <V(x)V(x')> correlator definition
-    S_analytic_corr, S_analytic_shuffle = [ S_analytic(kRange,a,delta,smearing) for a in alpha  ]  # analytical S(k)
+    S_analytic_corr, S_analytic_shuffle = [ S_analytic(kRange,a,delta,smearing[0]) for a in alpha  ]  # analytical S(k)
     
     # ---------------------------------------------
     # Transmission T(k), 1/L^(b,AGS), 1/L^(b,SGS)
@@ -169,7 +194,7 @@ if False:
     # ---------------------------------------------
     # Plot data: xi, W, S
     # ---------------------------------------------
-    p1 = plt.figure(figsize=(16,10))
+    p1 = plt.figure(figsize=(16,7))
     # compare step-profiles
     plt.subplot(311)
     plt.xlim(-0.2,0.2)
@@ -181,18 +206,18 @@ if False:
     plt.legend(loc=8,ncol=2)
     # compare W power spectra
     plt.subplot(312)
-    plt.xlim(0,300)
+    plt.xlim(0,120)
     plt.ylim(1e-12,1e-5)
-    plt.semilogy(kRange,W_analytic_shuffle,"r-",label="W(k) shuffled")
-    plt.semilogy(kRange,W_analytic_corr,"g-",label="W(k) correlated")
+    plt.semilogy(kRange/2,W_analytic_shuffle,"r-",label="W(k) shuffled")
+    plt.semilogy(kRange/2,W_analytic_corr,"g-",label="W(k) correlated")
     plt.legend(loc=8,ncol=2)
     # compare S power spectra
     plt.subplot(313)
     plt.xlabel("k",fontsize=14)
-    plt.xlim(0,300)
+    plt.xlim(0,120)
     plt.ylim(1e-6,1e2)
-    plt.semilogy(kRange,S_analytic_shuffle,"r-",label="S(k) shuffled")
-    plt.semilogy(kRange,S_analytic_corr,"g-",label="S(k) correlated")
+    plt.semilogy(kRange/2,S_analytic_shuffle,"r-",label="S(k) shuffled")
+    plt.semilogy(kRange/2,S_analytic_corr,"g-",label="S(k) correlated")
     plt.legend(loc=8,ncol=2)
 
     plt.show()
